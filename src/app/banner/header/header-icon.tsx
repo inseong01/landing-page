bee388d6d40ae2ccd9746a380840aaf496449486
inseong01 <insung940@naver.com>
2025-04-09 11:52:57 +0900

    Adopting Tailwind CSS
    - tailwind 도입
      - Vite Tailwind 플러그인 설정 "tailwindcss/vite"
      - 자동 포맷팅 확장 프로그램 설정 "Tailwind CSS IntelliSense"
      - 프리터 플러그인 설정 "prettier-plugin-tailwindcss"
    
    - 모듈에서 테일윈드 CSS 전환
    
    - 헤더 컴포넌트 모듈화

---
+++ src/app/banner/header/header-icon.tsx
@@ -0,0 +1,9 @@
+export default function Icon() {
+  return (
+    <a href="#">
+      <div className="flex h-full w-40 items-center justify-center">
+        Life Journal
+      </div>
+    </a>
+  );
+}
