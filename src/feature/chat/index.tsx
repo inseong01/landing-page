319eebdd1bd11567cf3e927d6fd41c14fdecfafb
Inseong <insung940@naver.com>
2025-09-22 14:26:37 +0900

    Revert "Revert "Fix github actions build error""

--- src/feature/chat/index.tsx
+++ src/feature/chat/index.tsx
@@ -153,7 +153,7 @@ export default function ChatApp() {
     return () => {
       MY_CHANNEL.unsubscribe();
     };
-  }, [ID, NAME, reconnection]);
+  }, [reconnection]);
 
   const visitorReceivedMsgCount = visitorState.messages.reduce((acc, msg) => {
     return acc + (!msg.payload.isRead && msg.payload.id !== ID ? 1 : 0);
